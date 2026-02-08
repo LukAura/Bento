9cb34d5be4f8e15482ba6d45c477d298082ff040
LukAura <isaacfppp@gmail.com>
2026-02-08 20:11:08 -0300

    Update weather API key and default location

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '288752458e9b0130f398aa44af330a02', // Write here your API Key
+	weatherKey: '340186bf13008ca2f4e95c8f9ef1f010', // Write here your API Key
 	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'pt', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '34.049',
-	defaultLongitude: '74.392',
+	defaultLatitude: '-19.80064',
+	defaultLongitude: '-43.95759',
 
 	// Autochange
 	autoChangeTheme: false,
